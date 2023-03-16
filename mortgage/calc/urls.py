c66ad8603e5a36d4c6f1620d272c94cd9ddef59a
Kanish <111358462+kanish13@users.noreply.github.com>
2023-03-16 21:20:48 +0530

    Delete mortgage directory

--- mortgage/calc/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('',views.index,name="index"),
-    path('monthly/',views.monthly,name="monthly"),
-    path('weekly/',views.weekly,name="weekly"),
-    path('daily/',views.daily,name="daily"),
-
-]
